c3d69122789b431317a25303681557bd8d0d131b
Johannes Tegnér <johannes@jitesoft.com>
2017-08-28 11:20:30 +0200

    Added base exception `JitesoftException` which all others will derive from.

---
+++ src/JitesoftException.php
@@ -0,0 +1,94 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  JitesoftException.php - Part of the php-exceptions project.
+
+  © - Jitesoft 2017
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Jitesoft\Exceptions;
+
+use Exception;
+
+/**
+ * Base class for Jitesoft Exception types.
+ */
+abstract class JitesoftException extends Exception {
+
+    /**
+     * Get the exception as a json object.
+     *
+     * <pre>
+     * {
+     *   'type'  => 'error_type',    (string)
+     *   'error' => 'error_message', (string)
+     *   'code'  => error_code,      (int)
+     *   'file'  => file_name,       (string)
+     *   'line'  => file_line,       (int)
+     *   'trace' => stack_trace,     (object)
+     *   'inner' => inner_exception  (object)
+     * }
+     * </pre>
+     *
+     * @return string
+     */
+    public function toJson() {
+        return json_encode($this->toArray());
+    }
+
+    /**
+     * Get the exception as an associative array.
+     *
+     * <pre>
+     * {
+     *   'type'  => 'error_type',    (string)
+     *   'error' => 'error_message', (string)
+     *   'code'  => error_code,      (int)
+     *   'file'  => file_name,       (string)
+     *   'line'  => file_line,       (int)
+     *   'trace' => stack_trace,     (array)
+     *   'inner' => inner_exception  (array)
+     * }
+     * </pre>
+     *
+     * @return array
+     */
+    public function toArray() {
+
+        $exception = [
+            'type'  => static::class,
+            'error' => $this->getMessage(),
+            'code'  => $this->getCode(),
+            'line'  => $this->getLine(),
+            'file'  => $this->getFile(),
+            'trace' => $this->getTrace(),
+            'inner' => null
+        ];
+
+        $prev  = $this->getPrevious();
+        $inner = &$arr['inner'];
+        while ($prev !== null) {
+
+            if ($prev instanceof JitesoftException) {
+                // If inner is a jitesoftexception, we can fetch the inner exception
+                // as an array, and due to the fact that there is only one inner exception,
+                // it will take care of the nesting in its own toArray method.
+                $inner = $prev->toArray();
+                break;
+            }
+
+            $inner = [
+                'type'  => get_class($prev),
+                'error' => $prev->getMessage(),
+                'code'  => $prev->getCode(),
+                'line'  => $prev->getLine(),
+                'file'  => $prev->getFile(),
+                'trace' => $prev->getTrace(),
+                'inner' => null
+            ];
+
+            $inner = &$inner['inner'];
+            $prev  = $this->getPrevious();
+        }
+
+        return $exception;
+    }
+}
