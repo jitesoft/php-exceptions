665bacd55d0c8a43fb7ad7e789fca67edce52c1a
Johannes Tegnér <johannes@jitesoft.com>
2017-09-04 11:26:46 +0200

    Added class DirectoryNotFoundException

---
+++ src/IOExceptions/DirectoryNotFundException.php
@@ -0,0 +1,13 @@
+<?php
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  DirectoryNotFundException.php - Part of the php-exceptions project.
+
+  © - Jitesoft 2017
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+namespace Jitesoft\Exceptions\IOExceptions;
+
+class DirectoryNotFundException extends IOException {
+
+}
